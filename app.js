e045c2bb4574ba90ccddf0f8d3c1ec67e6b9326c
DANNY QUAN <danielsah118@gmail.com>
2024-06-18 08:00:49 +0000

    applied rate limiting

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const ErrorHandlingMiddleware = require("./Middlewares/ErrorHandlingMiddleware");
-const mongoSanitize= require('express-mongo-sanitize')
-const helmet= require('helmet')
-const { xss } = require('express-xss-sanitizer');
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const { xss } = require("express-xss-sanitizer");
+const rateLimiter = require("express-rate-limit");
 
 const app = express();
 const userRoutes = require("./Routes/userRoutes");
 const fileRoutes = require("./Routes/FileRoutes");
+
+//rate limiter configuration here
+const limiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  message: "Too many requests from this IP, please try again in 15 minues time",
+  statusCode: 429,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,10 +28,10 @@ app.use(
     credentials: true,
   })
 );
-app.use(mongoSanitize()) // prevention agains noSql injection
-app.use(helmet()) // Setting HTTP response headers
-app.use(xss()) // prevention agains cross site scripting
-
+app.use(mongoSanitize()); // prevention agains noSql injection
+app.use(helmet()); // Setting HTTP response headers
+app.use(xss()); // prevention agains cross site scripting
+app.use(limiter); // applying rate litter to all requests
 
 //imported routes
 app.use("/api/v1/users", userRoutes);
