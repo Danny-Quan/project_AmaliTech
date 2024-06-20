ce5d8bd352404f39571ecc6bfc22b2ee837c1980
DANNY QUAN <danielsah118@gmail.com>
2024-06-20 15:42:05 +0000

    configured cors

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    // origin: "https://amalitech-lizzy-file-hub.netlify.app",
-    origin:"http://localhost:3000",
+    origin: "https://amalitech-lizzy-file-hub.netlify.app",
+    // origin:"http://localhost:3000",
     credentials: true,
   })
 );
